61b204be6de43036518803451eb8823a7b2b577a
ccnmagnoo <crcamposn@gmail.com>
2024-06-08 21:17:07 -0400

    copy line keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+--copy lines
+vim.keymap.set("n", "<S-A-Down>", "yyp", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-A-Down>", "yyp", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-A-Down>", "yyp", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-A-Up>", "yyP", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-z>", "<C-o>u", { noremap = true, silent = true })
