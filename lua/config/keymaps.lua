7277ced004677818c2dfc211fe3f95f6722a699a
ccnmagnoo <crcamposn@gmail.com>
2024-06-10 16:54:55 -0400

    update keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,13 +3,13 @@
 -- Add any additional keymaps here
 
 --copy lines
-vim.keymap.set("n", "<A-Down>", "yyp", { noremap = true, silent = true }) --copy line Down
-vim.keymap.set("n", "<A-Up>", "yyP", { noremap = true, silent = true }) --copy line Up
+vim.keymap.set("n", "<S-Down>", "yyp", { noremap = true, silent = true }) --copy line Down
+vim.keymap.set("n", "<S-Up>", "yyP", { noremap = true, silent = true }) --copy line Up
 -- basic
 vim.keymap.set("i", "<C-z>", "<C-o>u", { noremap = true, silent = true }) --ctrl z
 vim.keymap.set("n", "<C-z>", "<C-o>u", { noremap = true, silent = true }) --ctrl z
 --move lines
-vim.keymap.set("n", "<C-Up>", ":m .-2<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-Down>", ":m .+1<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>", { noremap = true, silent = true })
 --jump
 vim.keymap.set("i", "<C><CR>", "<End><CR>", { noremap = true, silent = true })
